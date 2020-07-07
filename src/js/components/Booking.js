656ce0cceb442c8d615a2df05e50d164455a569b
Krzysztof Fila <kfila17@gmail.com>
2020-07-07 21:58:36 +0200

    add BookingTable

--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -1,8 +1,8 @@
 import { templates, select, settings, classNames } from '../settings.js';
-import utils from '../utils.js';
-import AmountWidget from './AmountWidget.js';
-import DatePicker from './DatePicker.js';
-import HourPicker from './HourPicker.js';
+import { utils } from '../utils.js';
+import { AmountWidget } from './AmountWidget.js';
+import { DatePicker } from './DatePicker.js';
+import { HourPicker } from './HourPicker.js';
 export class Booking {
   constructor(element) {
     const thisBooking = this;
